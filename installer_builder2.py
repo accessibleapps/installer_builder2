c72f7ec8919e977347b97d44191e904c7ded1273
Christopher Toth <q.alpha@gmail.com>
2022-09-09 18:05:21 -0600

    Support for --nofollow-imports-to for Nuitka and refactored how nuitka is called.

--- installer_builder2.py
+++ installer_builder2.py
@@ -22,6 +22,7 @@ class InstallerBuilder:
     company_name: str = field(default='')
     include_modules: list = field(default=Factory(list), converter=list)
     include_packages: list = field(default=Factory(list), converter=list)
+    ignore_imports: list = field(default=Factory(list), converter=list)
     data_files: list = field(default=Factory(list), converter=list)
     data_directories: list = field(default=Factory(list), converter=list)
     data_file_packages: list = field(default=Factory(list), converter=list)
@@ -31,7 +32,7 @@ class InstallerBuilder:
 
     def compile_distribution(self):
         run_nuitka(self.main_module, self.dist_path, app_name=self.app_name, app_version=self.version, company_name=self.company_name,
-                   include_modules=self.include_modules, include_data_files=self.data_files, include_data_dirs=self.data_directories, packages_to_include=self.include_packages, data_file_packages=self.data_file_packages)
+                   include_modules=self.include_modules, include_data_files=self.data_files, include_data_dirs=self.data_directories, packages_to_include=self.include_packages, data_file_packages=self.data_file_packages, ignore_imports=self.ignore_imports)
 
     def create_installer(self):
         import innosetup_builder
@@ -101,7 +102,7 @@ class InstallerBuilder:
         self.create_update_zip()
 
 
-def run_nuitka(main_module, output_path=pathlib.Path('dist'), include_modules=None, packages_to_include=None, console=False, onefile=False, include_data_files=None, include_data_dirs=None, app_name="", company_name="", app_version="", numpy=False, data_file_packages=None):
+def run_nuitka(main_module, output_path=pathlib.Path('dist'), include_modules=None, packages_to_include=None, console=False, onefile=False, include_data_files=None, include_data_dirs=None, app_name="", company_name="", app_version="", numpy=False, data_file_packages=None, ignore_imports=None):
     if include_modules is None:
         include_modules = []
     include_modules = ['--include-module=' +
@@ -122,30 +123,34 @@ def run_nuitka(main_module, output_path=pathlib.Path('dist'), include_modules=No
         data_file_packages = []
     data_file_packages = ['--include-package-data=' +
                           package for package in data_file_packages]
+    if ignore_imports is None:
+        ignore_imports = []
+    ignore_imports = ['--nofollow-import-to=' +
+                      ignore for ignore in ignore_imports]
     extra_options = ['--assume-yes-for-downloads',
                      '--output-dir=' + str(output_path)]
-    command = [sys  .executable, '-m', 'nuitka', '--standalone', *include_modules,
-               *include_packages, *include_data_files, *include_data_dirs, *data_file_packages, *extra_options, main_module]
     if onefile:
-        command.append('--onefile')
+        extra_options.append('--onefile')
     if not console:
-        command.append('--windows-disable-console')
-        command.append('--macos-disable-console')
-        command.append('--macos-create-app-bundle')
+        extra_options.append('--windows-disable-console')
+        extra_options.append('--macos-disable-console')
+        extra_options.append('--macos-create-app-bundle')
     if company_name:
-        command.append('--windows-company-name="' + company_name + '"')
+        extra_options.append('--windows-company-name="' + company_name + '"')
     if app_name:
-        command.append('--windows-product-name="' + app_name + '"')
-        command.append('--macos-app-name=' + app_name)
+        extra_options.append('--windows-product-name="' + app_name + '"')
+        extra_options.append('--macos-app-name=' + app_name)
     if app_version:
         # windows app versions must be in the format 1.0.0.0
         windows_version = app_version.split('.')
         windows_version.extend(['0'] * (4 - len(windows_version)))
-        command.append('--windows-product-version=' +
-                       '.'.join(windows_version))
-        command.append('--macos-app-version=' + app_version)
+        extra_options.append('--windows-product-version=' +
+                             '.'.join(windows_version))
+        extra_options.append('--macos-app-version=' + app_version)
     if numpy:
-        command.append('--enable-plugin=numpy')
+        extra_options.append('--enable-plugin=numpy')
+    command = [sys  .executable, '-m', 'nuitka', '--standalone', *include_modules,
+               *include_packages, *include_data_files, *include_data_dirs, *data_file_packages, *ignore_imports, *extra_options, main_module]
     subprocess.check_call(command)
 
 
